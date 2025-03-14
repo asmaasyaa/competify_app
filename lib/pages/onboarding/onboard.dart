a9c64c76fb5d23550adba8d90a1e00d740845351
asma <syakiraasmaaa@gamil.com>
2025-03-14 22:09:07 +0700

    Add missing onboarding.dart

---
+++ lib/pages/onboarding/onboard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Onboard extends StatefulWidget {
+  const Onboard({super.key});
+
+  @override
+    State<Onboard> createState() => _Onboard();
+}
+
+class _Onboard extends State<Onboard> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
